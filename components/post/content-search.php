27bff7744e5ffa681d438d1f1c603f533d48e023
LebCit <lebcit@gmail.com>
2017-05-13 16:11:46 +0300

    Changed content-meta place.

--- components/post/content-search.php
+++ components/post/content-search.php
@@ -12,13 +12,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-
-        <?php if ('post' === get_post_type()) : ?>
-            <?php get_template_part('components/post/content', 'meta'); ?>
-        <?php endif; ?>
     </header>
     <div class="entry-summary">
         <?php the_excerpt(); ?>
     </div>
+    <?php if ('post' === get_post_type()) : ?>
+        <?php get_template_part('components/post/content', 'meta'); ?>
+    <?php endif; ?>
 
 </article>
