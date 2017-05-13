7ad5a0af5238e8ff1a7f8372b96f0616e4774656
LebCit <lebcit@gmail.com>
2017-05-13 16:06:01 +0300

    Added Site Title And Description. Removed _unprefix_subtitle for approval.

--- components/header/bg-header.php
+++ components/header/bg-header.php
@@ -13,8 +13,10 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="site-heading">
                     <header class="entry-header">
+                        <?php if (is_front_page() && is_home()) { ?>
+                        <h1 id="responsive_headline" class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
                         <?php
-                        if (is_single() || is_page() || is_sticky() || is_front_page()) {
+                        } elseif (is_single() || is_page() || is_sticky()) {
                             the_title('<h1 class="entry-title">', '</h1>');
                         } else {
                             the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
@@ -29,14 +31,15 @@
                         </div>
 
                         <h2 class="subheading">
+                            <?php if (is_front_page() && is_home()) {
+                                $description = get_bloginfo('description', 'display');
+                                if ($description || is_customize_preview()) :
+                            ?>
+                                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
                             <?php
-                            if ('post' || 'page' === get_post_type()) :
-                                global $wp_query;
-                                $postid = $wp_query->post->ID;
-                                echo esc_html(get_post_meta($postid, '_unprefix_subtitle', true));
-                                wp_reset_query();
-                            endif;
-
+                            endif; } else {
+                                cleanblog_posted_on();
+                            }
                             ?>
                         </h2>
                     </header>                                             
