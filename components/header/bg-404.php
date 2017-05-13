aafc56b37ecd195c74ff63ad8c8a72be2adc6555
LebCit <lebcit@gmail.com>
2017-05-13 15:53:52 +0300

    Escaping Output. Removed Inline Styles.

--- components/header/bg-404.php
+++ components/header/bg-404.php
@@ -6,29 +6,18 @@
 ?>
 <!-- Set background image for this header . -->
 <header id="masthead" class="site-header intro-header" role="banner">
-    <style>
-        .intro-header {
-            height: 100vh;
-            background-image: url('<?php
-            echo esc_url(get_template_directory_uri());
-            echo '/components/header/images/404-hero.jpg';
-
-            ?>');
-        }
-    </style>
-
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="site-heading">
                     <header class="entry-header">
-                        <h1 class="entry-title">Wrong Archives Row !</h1>
+                        <h1 class="entry-title"><?php esc_html_e('Wrong Archives Row !', 'the-clean-blog') ?></h1>
                         <div class="strike">
                             <span>
                                 <a href="#content"><i class="fa fa-arrow-down" aria-hidden="true"></i></a>
                             </span>
                         </div>
-                        <h2 class="subheading">Try to search below</h2>
+                        <h2 class="subheading"><?php esc_html_e('Try to search below', 'the-clean-blog') ?></h2>
                     </header>
                 </div>
             </div>
