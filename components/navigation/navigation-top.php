3bd46652b1a7f17e078cef9186d7221032f2bc98
LebCit <lebcit@gmail.com>
2017-05-13 16:07:12 +0300

    Modified For New Navigation.

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -1,26 +1,16 @@
-<div class="container-fluid">
+<div class="cb-logo">
+    <?php cleanblog_the_custom_logo(); ?>
+</div>
 
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header page-scroll">
-
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">                
-            <i class="fa fa-bars" aria-hidden="true"></i>
-        </button>
-        <button class="search-trigger"><i class="icon-search"></i></button>
-
-        <?php get_template_part('components/header/site', 'branding'); ?>
-
-    </div><!-- /.navbar-header.page-scroll -->
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> 
-
-        <ul class="nav navbar-nav navbar-right">
-            <?php wp_nav_menu(array('theme_location' => 'menu-1', 'container' => false, 'items_wrap' => '%3$s')); ?>
-            <li class="search-trigger"><i class="icon-search"></i></li>
-        </ul>
-    </div><!-- /.navbar-collapse -->
-
+    <?php
+        wp_nav_menu(array(
+            'theme_location' => 'menu-1',
+            'container' => 'nav',
+            'container_class' => 'cb-main-nav-wrapper',
+            'menu_class' => 'cb-main-nav',
+            'depth' => 2,
+        ));
+    ?>
     <div class="search-dropdown">
         <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
             <label>
@@ -36,5 +26,4 @@
             </button>
         </form>
     </div>
-
-</div><!-- /.container-fluid -->
+<a href="#0" class="cb-nav-trigger">Menu<span></span></a>
