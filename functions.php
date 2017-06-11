19c4eaed1d4425609c93939b7397d38efcfde082
LebCit <lebcit@gmail.com>
2017-06-12 01:53:37 +0300

    Remove the REMOVE WP EMOJI ACTIONS
    Removing the actions of removing wp emoji.

--- functions.php
+++ functions.php
@@ -113,15 +113,6 @@ function thecleanblog_the_custom_logo() {
 }
 
 /**
- * REMOVE WP EMOJI
- */
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-
-remove_action('admin_print_scripts', 'print_emoji_detection_script');
-remove_action('admin_print_styles', 'print_emoji_styles');
-
-/**
  * Enqueue scripts and styles.
  */
 function thecleanblog_scripts()
