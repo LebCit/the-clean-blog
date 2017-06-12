a1c811a7a4be92b3272ef06acdfc5e57a0def84c
LebCit <lebcit@gmail.com>
2017-06-13 02:37:18 +0300

    Add Prefix To google Fonts
    Google Fonts are not working without prefix !

--- functions.php
+++ functions.php
@@ -126,9 +126,9 @@ function thecleanblog_scripts()
     
     wp_enqueue_style('font-awesome', get_theme_file_uri('/fa/css/font-awesome.min.css'));
 
-    wp_enqueue_style('lora', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
+    wp_enqueue_style('thecleanblog-lora', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
 
-    wp_enqueue_style('open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
+    wp_enqueue_style('thecleanblog-open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
 
     wp_enqueue_script('thecleanblog-navigation', get_theme_file_uri('/assets/js/navigation.js'), array(), '20151215', true);
 
